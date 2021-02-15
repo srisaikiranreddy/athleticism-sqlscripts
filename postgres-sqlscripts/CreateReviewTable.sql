765324f5397b61b4b74ab5544b80d5ec78586581
SaiKiran Palle <srisaikiranreddy@gmail.com>
2021-02-15 18:14:00 -0500

    reviews table

---
+++ postgres-sqlscripts/CreateReviewTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE reviews(
+   id INT PRIMARY KEY     NOT NULL,
+   title varchar(20),
+   review_comment varchar(100)   
+);
+
---
+++ postgres-sqlscripts/InsertData_Reviews.sql
@@ -0,0 +1,7 @@
+Insert into reviews(id,title, review_comment) values(1,'sample','sample comment');
+Insert into reviews(id,title, review_comment) values(2,'sample2','sample comment 2');
+
+
+
+
+
